c48a4ba14f30cc2b230dd84b14f2b9e78e72928e
samuel <samueltekle2023@gmail.com>
2023-11-21 13:16:31 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const app=express()
+const port=5000
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+
+    res.render('homepage')
+})
+
+app.get('/blogs',(req,res)=>{
+
+    res.render('blogList')
+})
+
+app.get('/myblogs',(req,res)=>{
+
+    res.render('myblogs')
+})
+
+app.get('/blog',(req,res)=>{
+
+    res.render('blog')
+})
+
+
+app.listen(port,()=>console.log(`listening on http://localhost:${port}`))
